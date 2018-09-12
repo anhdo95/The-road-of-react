eb385f2f35af563961da089f9188f23250dab78a
AnhDo <dinhanhdo95@gmail.com>
2018-09-12 22:42:11 +0700

    Usage PropTypes

--- src/Table.js
+++ src/Table.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "./Button";
+import PropTypes from 'prop-types';
 
 const largeColumn = {
   width: "40%"
@@ -36,5 +37,18 @@ const Table = ({ list, onDissmiss }) => (
     ))}
   </div>
 );
+
+Table.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      objectID: PropTypes.string.isRequired,
+      author: PropTypes.string,
+      url: PropTypes.string,
+      num_comments: PropTypes.number,
+      points: PropTypes.number
+    })
+  ).isRequired,
+  onDissmiss: PropTypes.func.isRequired
+};
 
 export default Table;
